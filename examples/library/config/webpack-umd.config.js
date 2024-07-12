c2df2174d01651d26e6c3083fd27eae11ae41383
Strawhat <strawhat.x@qq.com>
2024-07-12 15:52:00 +0800

    feat: library externals

--- examples/library/config/webpack-umd.config.js
+++ examples/library/config/webpack-umd.config.js
@@ -9,12 +9,22 @@ const resolveApp = (relativePath) => path.resolve(appDir, relativePath);
 export default {
   entry: resolveApp('src/index.js'),
   output: {
+    clean: true,
     path: resolveApp('dist'),
     filename: '[name].[contenthash:8].umd.js',
+    globalObject: 'this',
     library: {
       name: 'hell-world',
       type: 'umd',
     },
+  },
+  externals: {
+    'lodash-es': {
+      commonjs: 'lodash-es',
+      commonjs2: 'lodash-es',
+      amd: 'lodash-es',
+      root: '_'
+    }
   },
   optimization: {
     minimize: false,
--- examples/library/src/index.js
+++ examples/library/src/index.js
@@ -1,3 +1,6 @@
+import { flatten } from 'lodash-es';
 import { helloWorld } from './hello-world.js';
+
+console.log('externals lodash', flatten([[0], [1]]).join(' '));
 
 export default function () { console.log(helloWorld); }
